5edfc03676f8085e998cb6ec95dda924bf8b7592
gabe <gabe@hpe.com>
2022-02-03 23:01:44 +0100

    feat: hiding away complexity of Duration and Period calculations

---
+++ src/main/java/com/hpe/tslabs/java/basics/enumerations/DateAndTimeOffsetCalculation.java
@@ -0,0 +1,173 @@
+// (c) Copyright 2022 Hewlett-Packard Development Company, L.P.
+package com.hpe.tslabs.java.basics.enumerations;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.Period;
+import java.time.ZonedDateTime;
+import java.time.temporal.TemporalAmount;
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * com.hpe.tslabs.java.basics.enumerations
+ * <p>
+ * Author: Gabriel Inäbnit - 2022-02-03
+ * <p>
+ * {@link java.time.Duration}: {@code PnDTnHnMn.nS}
+ * <p>
+ * {@link java.time.Period}: {@code PnYnMnD}
+ * <p>
+ * We cannot add/subtract months or years this way. When attempted we get this exception:
+ * java.time.temporal.UnsupportedTemporalTypeException: Unsupported unit: Months
+ * <p>
+ * Explanation why we need to make an extra effort to calculate years and months and inspiration for this
+ * implementation:
+ *
+ * @see <a href="https://stackoverflow.com/questions/39907925/why-instant-does-not-support-operations-with-chronounit-years/49252497">why-instant-does-not-support-operations-with-chronounit-years</a>
+ * @see <a href="https://stackoverflow.com/questions/30833582/how-to-calculate-the-number-of-days-in-a-period">how-to-calculate-the-number-of-days-in-a-period</a>
+ */
+public enum DateAndTimeOffsetCalculation
+{
+
+   //@formatter:off
+   NANOS          ("nanos",      Duration.class,   "ofNanos",     Long.TYPE),
+   MILLIS         ("millis",     Duration.class,   "ofMillis",    Long.TYPE),
+   SECONDS        ("seconds",    Duration.class,   "ofSeconds",   Long.TYPE),
+   MINUTES        ("minutes",    Duration.class,   "ofMinutes",   Long.TYPE),
+   HOURS          ("hours",      Duration.class,   "ofHours",     Long.TYPE),
+   DAYS           ("days",       Period.class,     "ofDays",      Integer.TYPE),
+   WEEKS          ("weeks",      Period.class,     "ofWeeks",     Integer.TYPE),
+   MONTHS         ("months",     Period.class,     "of",          Integer.TYPE, Integer.TYPE, Integer.TYPE),
+   YEARS          ("years",      Period.class,     "of",          Integer.TYPE, Integer.TYPE, Integer.TYPE),
+   MONTHS_AS_IS   ("monthsAsIs", Period.class,     "ofMonths",    Integer.TYPE),
+   YEARS_AS_IS    ("yearsAsIs",  Period.class,     "ofYears",     Integer.TYPE),
+   DURATION       ("duration",   Duration.class,   "parse",       CharSequence.class),
+   PERIOD         ("period",     Period.class,     "parse",       CharSequence.class),
+   // This will simply log an error
+   BOOM           ("boom",       Period.class,     "boom",        CharSequence.class),
+   ;
+   //@formatter:on
+
+   private static final Logger logger = LoggerFactory.getLogger(DateAndTimeOffsetCalculation.class);
+
+   private static final Map<String, DateAndTimeOffsetCalculation> lookup = new HashMap<>();
+
+   static
+   {
+      for (final DateAndTimeOffsetCalculation lambda : EnumSet.allOf(DateAndTimeOffsetCalculation.class))
+      {
+         lookup.put(lambda.units, lambda);
+      }
+   }
+
+   private final String units;
+   private final Method method;
+
+   /**
+    * Enum constructor
+    *
+    * @param units      name of unit
+    * @param clazz      java.time class
+    * @param methodName to be invoked on clazz
+    * @param valueType  value type used to find method
+    */
+   DateAndTimeOffsetCalculation(final String units, final Class<?> clazz, final String methodName,
+           final Class<?>... valueType)
+   {
+      Method establishMethod;
+      this.units = units;
+      try
+      {
+         establishMethod = clazz.getDeclaredMethod(methodName, valueType);
+      }
+      catch (final NoSuchMethodException e)
+      {
+         establishMethod = null;
+         final Logger constructorLogger = LoggerFactory.getLogger(DateAndTimeOffsetCalculation.class);
+         constructorLogger.error("Class {} has no method named {}", clazz.getSimpleName(), methodName);
+      }
+      this.method = establishMethod;
+   }
+
+   /**
+    * We're hiding (maybe) the complexity of the choice between Duration and Period
+    *
+    * @param from   ISO date and time
+    * @param amount of units (positive or negative)
+    * @param units  name
+    * @return ISO date and time with offset applied
+    */
+   public static String getResultingDateAndTime(final String from, final int amount, final String units)
+   {
+      final DateAndTimeOffsetCalculation function = lookup.get(units);
+      try
+      {
+         Objects.requireNonNull(function.method);
+         if ("years".equalsIgnoreCase(units))
+         {
+            return ZonedDateTime.parse(from).plus((TemporalAmount) function.method.invoke(null, amount, 0, 0))
+                    .toString();
+         }
+         else if ("months".equalsIgnoreCase((units)))
+         {
+            return ZonedDateTime.parse(from).plus((TemporalAmount) function.method.invoke(null, 0, amount, 0))
+                    .toString();
+         }
+         else
+         {
+            return Instant.parse(from).plus((TemporalAmount) function.method.invoke(null, amount)).toString();
+         }
+      }
+      catch (final NullPointerException e)
+      {
+         logger.error("No methods exists for units: {}", units);
+      }
+      catch (final IllegalAccessException | InvocationTargetException e)
+      {
+         logger.error("{}", e.getMessage());
+      }
+      return from;
+   }
+
+   /**
+    * Offsetting a given date and time with a period or duration
+    *
+    * @param from   ISO data and time
+    * @param period {@link java.time.Duration} {@code PnDTnHnMn.nS} or {@link java.time.Period} {@code PnYnMnD}
+    * @param units  duration or period
+    * @return ISO date and time with offset applied
+    */
+   public static String getResultingDateAndTime(final String from, final String period, final String units)
+   {
+      final DateAndTimeOffsetCalculation function = lookup.get(units);
+      try
+      {
+         switch (function.units)
+         {
+            case "duration":
+            case "period":
+               return ZonedDateTime.parse(from).plus((TemporalAmount) function.method.invoke(null, period)).toString();
+            default:
+               return from;
+         }
+      }
+      catch (final NullPointerException e)
+      {
+         logger.error("No parse method exists for {}", units);
+      }
+      catch (final IllegalAccessException | InvocationTargetException e)
+      {
+         logger.error("{}", e.getMessage());
+      }
+      return from;
+   }
+
+}
---
+++ src/test/java/com/hpe/tslabs/java/basics/enumerations/DateAndTimeOffsetCalculationTest.java
@@ -0,0 +1,193 @@
+// (c) Copyright 2022 Hewlett-Packard Development Company, L.P.
+package com.hpe.tslabs.java.basics.enumerations;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.InvocationTargetException;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.Period;
+import java.time.chrono.IsoChronology;
+import java.time.temporal.UnsupportedTemporalTypeException;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * com.hpe.tslabs.java.basics.enumerations
+ * <p>
+ * Author: Gabriel Inäbnit - 2022-02-03
+ */
+class DateAndTimeOffsetCalculationTest
+{
+
+   final String  sample  = "2022-02-02T12:34:56.789Z";
+   final Instant instant = Instant.parse(sample);
+
+   @Test
+   void validate_parse_to_string()
+   {
+      assertEquals(sample, instant.toString());
+   }
+
+   @Test
+   void add_3_minutes_to_from()
+   {
+      final String resultingDateAndTime = DateAndTimeOffsetCalculation.getResultingDateAndTime(sample, 3, "minutes");
+      assertEquals("2022-02-02T12:37:56.789Z", resultingDateAndTime);
+   }
+
+   @Test
+   void add_3_days_to_from()
+   {
+      final String resultingDateAndTime = DateAndTimeOffsetCalculation.getResultingDateAndTime(sample, 3, "days");
+      assertEquals("2022-02-05T12:34:56.789Z", resultingDateAndTime);
+   }
+
+   @Test
+   void subtract_3_days_to_from()
+   {
+      final String resultingDateAndTime = DateAndTimeOffsetCalculation.getResultingDateAndTime(sample, -3, "days");
+      assertEquals("2022-01-30T12:34:56.789Z", resultingDateAndTime);
+   }
+
+   @Test
+   void add_300_weeks_to_from()
+   {
+      final String resultingDataAndTime = DateAndTimeOffsetCalculation.getResultingDateAndTime(sample, 300, "weeks");
+      assertEquals("2027-11-03T12:34:56.789Z", resultingDataAndTime);
+   }
+
+   @Test
+   void add_3_months_to_from()
+   {
+      final String resultingDataAndTime = DateAndTimeOffsetCalculation.getResultingDateAndTime(sample, 3, "months");
+      assertEquals("2022-05-02T12:34:56.789Z", resultingDataAndTime);
+   }
+
+   @Test
+   void subtract_3_months_to_from()
+   {
+      final String resultingDataAndTime = DateAndTimeOffsetCalculation.getResultingDateAndTime(sample, -3, "months");
+      assertEquals("2021-11-02T12:34:56.789Z", resultingDataAndTime);
+   }
+
+   @Test
+   void add_3_years_to_from()
+   {
+      final String resultingDataAndTime = DateAndTimeOffsetCalculation.getResultingDateAndTime(sample, 3, "years");
+      assertEquals("2025-02-02T12:34:56.789Z", resultingDataAndTime);
+   }
+
+   @Test
+   void add_months_to_from_exception()
+   {
+      UnsupportedTemporalTypeException thrown = Assertions.assertThrows(UnsupportedTemporalTypeException.class,
+              () -> DateAndTimeOffsetCalculation.getResultingDateAndTime(sample, 3, "monthsAsIs"));
+      Assertions.assertEquals("Unsupported unit: Months", thrown.getMessage());
+   }
+
+   /**
+    * NOTE: gracefully handling wrong data and simply returning the input string
+    */
+   @Test
+   void no_such_method_exception()
+   {
+      assertEquals(sample, DateAndTimeOffsetCalculation.getResultingDateAndTime(sample, 3, "boom"));
+   }
+
+   @Test
+   void add_years_to_from_exception()
+   {
+      UnsupportedTemporalTypeException thrown = Assertions.assertThrows(UnsupportedTemporalTypeException.class,
+              () -> DateAndTimeOffsetCalculation.getResultingDateAndTime(sample, 3, "yearsAsIs"));
+      Assertions.assertEquals("Unsupported unit: Years", thrown.getMessage());
+   }
+
+   //@Test
+   void duration_stuff() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException
+   {
+      // all methods of Duration
+      Arrays.stream(Duration.class.getMethods()).forEach(System.out::println);
+      // ofMinutes method of Duration
+      System.out.println(Duration.class.getMethod("ofMinutes", Long.TYPE));
+      // execute ofMinutes method of Duration - notice the first argument of invoke: null = static method
+      System.out.println(Duration.class.getMethod("ofMinutes", Long.TYPE).invoke(null, 123));
+   }
+
+   //@Test
+   void period_stuff() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException
+   {
+      // all methods of Duration
+      Arrays.stream(Period.class.getMethods()).forEach(System.out::println);
+      // ofMinutes method of Duration
+      System.out.println(Period.class.getMethod("ofYears", int.class));
+      // execute ofMinutes method of Duration - notice the first argument of invoke: null = static method
+      System.out.println(Period.class.getMethod("ofYears", Integer.TYPE).invoke(null, 123));
+   }
+
+   /**
+    * understanding period representation
+    */
+   @Test
+   void get_days_from_a_period()
+   {
+      final Period period = Period.of(1, 2, 3);
+      assertEquals(1, period.getYears());
+      assertEquals(2, period.getMonths());
+      assertEquals(3, period.getDays());
+      assertEquals(IsoChronology.INSTANCE, period.getChronology());
+      assertEquals("P1Y2M3D", period.toString());
+   }
+
+   /**
+    * understanding duration representation
+    */
+   @Test
+   void get_days_from_a_duration()
+   {
+      final Duration duration = Duration.ofMillis(1234567890);
+      assertEquals(1234567, duration.getSeconds());
+      assertEquals("PT342H56M7.89S", duration.toString());
+   }
+
+   @Test
+   void add_14_hours_to_from()
+   {
+      assertEquals("2022-02-03T02:34:56.789Z",
+              DateAndTimeOffsetCalculation.getResultingDateAndTime(sample, "PT14H", "duration"));
+
+   }
+
+   @Test
+   void subtract_14_hours_to_from()
+   {
+      assertEquals("2022-02-01T22:34:56.789Z",
+              DateAndTimeOffsetCalculation.getResultingDateAndTime(sample, "PT-14H", "duration"));
+
+   }
+
+   /**
+    * observe that adding 1 month to January 31st results in returning the last day of February
+    */
+   @Test
+   void add_1_month_to_last_day_in_january()
+   {
+      assertEquals("2022-02-28T12:24:56Z",
+              DateAndTimeOffsetCalculation.getResultingDateAndTime("2022-01-31T12:24:56Z", "P1M", "period"));
+
+   }
+
+   /**
+    * observe the taking care of the leap year
+    */
+   @Test
+   void add_1_month_to_last_day_in_january_of_2020()
+   {
+      assertEquals("2020-02-29T12:24:56Z",
+              DateAndTimeOffsetCalculation.getResultingDateAndTime("2020-01-31T12:24:56Z", "P1M", "period"));
+
+   }
+
+}
